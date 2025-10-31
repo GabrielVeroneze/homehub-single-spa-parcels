56a91f695262de06d7e97469aef151660d288d1a
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-10-31 16:04:55 -0300

    ♻️ refactor: remove integração do Parcel do App de login
    Remove o componente `<Parcel />` do single-spa da tela de login, simplificando a interface e retirando a renderização do microfrontend `@homehub/react-parcel` nesse contexto.

--- react-login/src/App.tsx
+++ react-login/src/App.tsx
@@ -1,5 +1,4 @@
 import { Box } from '@mui/material'
-import Parcel from 'single-spa-react/parcel'
 import LoginBox from './components/LoginBox'
 import backgroundImg from './assets/background.png'
 
@@ -15,14 +14,6 @@ const App = () => {
             }}
         >
             <LoginBox />
-            <Parcel
-                config={() =>
-                    import(
-                        /* webpackIgnore: true */ // @ts-ignore-next
-                        '@homehub/react-parcel'
-                    )
-                }
-            />
         </Box>
     )
 }
