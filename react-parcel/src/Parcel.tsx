83164b3005362026964f9c061ab00df860b1dea2
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-10-31 15:20:18 -0300

    ✨ feat: implementa componente Parcel com diálogo interativo
    Aprimora o componente Parcel  para exibir um diálogo modal utilizando os componentes do Material UI, com suporte a transição animada via Slide. Agora ele recebe as propriedades title, description, leftBtnText e rightBtnText, permitindo personalização do conteúdo e dos botões de ação, além de gerenciar seu estado interno de abertura e fechamento.

--- react-parcel/src/Parcel.tsx
+++ react-parcel/src/Parcel.tsx
@@ -1,5 +1,69 @@
-const Parcel = () => {
-    return <section>Hello World!</section>
+import { forwardRef, useState } from 'react'
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    Slide,
+} from '@mui/material'
+import { TransitionProps } from '@mui/material/transitions'
+
+const Transition = forwardRef(function Transition(
+    props: TransitionProps & {
+        children: React.ReactElement<any, any>
+    },
+    ref: React.Ref<unknown>
+) {
+    return <Slide direction="up" ref={ref} {...props} />
+})
+
+interface ParcelProps {
+    title: string
+    description: string
+    leftBtnText: string
+    rightBtnText: string
+}
+
+const Parcel = ({
+    title,
+    description,
+    leftBtnText,
+    rightBtnText,
+}: ParcelProps) => {
+    const [open, setOpen] = useState<boolean>(false)
+
+    const handleClickOpen = () => {
+        setOpen(true)
+    }
+
+    const handleClose = () => {
+        setOpen(false)
+    }
+
+    return (
+        <Dialog
+            open={open}
+            slots={{
+                transition: Transition,
+            }}
+            keepMounted
+            onClose={handleClose}
+            aria-describedby="alert-dialog-slide-description"
+        >
+            <DialogTitle>{title}</DialogTitle>
+            <DialogContent>
+                <DialogContentText id="alert-dialog-slide-description">
+                    {description}
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>{leftBtnText}</Button>
+                <Button onClick={handleClose}>{rightBtnText}</Button>
+            </DialogActions>
+        </Dialog>
+    )
 }
 
 export default Parcel
